1fe1b7f94a28687fdfd29eaf491555157706138d
Sepehr Safari <safari.sepehr@gmail.com>
2023-04-19 13:27:06 +0330

    fix re render issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { matchFilters } from 'nostr-tools';
 
 import { useNostrStore } from './store';
 
 import { Config } from './types';
 
+import { generateSubId } from './utils';
+
 export * from './types';
 export * as utils from './utils';
 
 export const useNostr = ({ filters, relays, options }: Config) => {
+  const subId = useRef<string>(generateSubId());
+
   const sub = useNostrStore((store) => store._handleNewSub);
-
-  useEffect(() => {
-    if (options?.enabled === undefined || options?.enabled === true) {
-      sub({ filters, relays, options });
-    }
-  }, [options?.enabled]);
-
+  const purgeEvents = useNostrStore((store) => store._purgeEvents);
   const events = useNostrStore(
     (store) => store._events.filter((event) => matchFilters(filters, event)),
     (prev, next) => {
-      return prev.length === next.length;
+      const matchingPrev = prev.filter((event) => matchFilters(filters, event));
+      const matchingNext = next.filter((event) => matchFilters(filters, event));
+
+      if (matchingPrev.length !== matchingNext.length) {
+        return false;
+      }
+
+      // every event ids in next must be in prev to return true and avoid re-render
+      const isTheSame = matchingNext.every((nextEvent) => {
+        return matchingPrev.some((prevEvent) => prevEvent.id === nextEvent.id);
+      });
+
+      return isTheSame;
     }
   );
+
+  useEffect(() => {
+    if (options?.enabled === undefined || options?.enabled === true) {
+      sub({ filters, relays, options }, subId.current);
+    }
+
+    return () => {
+      purgeEvents(subId.current);
+    };
+  }, [options?.enabled]);
 
   return {
     events,
