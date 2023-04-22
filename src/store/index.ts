f98728c118dde0fad1c5d89540adae10fa151e3f
Sepehr Safari <safari.sepehr@gmail.com>
2023-04-22 16:43:22 +0330

    refactor store and use records

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,15 +6,15 @@ import { mergeFilters } from '../utils';
 import { Config } from '../types';
 import _ from 'lodash';
 
-type Queue = { config: Config; subId: string }[];
-type SubList = { id: string; filters: Filter[]; eose: boolean }[];
+type Queue = Record<string, Config>;
+type SubMap = Record<string, { filters: Filter[]; eose: boolean }>;
 
 interface State {
   pool: SimplePool;
   events: Event[];
   queue: Queue;
   isBatching: boolean;
-  subList: SubList;
+  subMap: SubMap;
 }
 
 interface Actions {
@@ -35,12 +35,14 @@ interface Actions {
 export const useNostrStore = create<State & Actions>()((set, get) => ({
   pool: new SimplePool(),
   events: [],
-  queue: [],
-  subList: [],
+  queue: {},
+  subMap: {},
   isBatching: false,
   setEose: (subIds) => {
     set((store) => ({
-      subList: store.subList.map((sub) => (subIds.includes(sub.id) ? { ...sub, eose: true } : sub)),
+      subMap: _.mapValues(store.subMap, (val, key) =>
+        subIds.includes(key) ? { ...val, eose: true } : val
+      ),
     }));
   },
   unSub: (subId) => {
@@ -48,18 +50,16 @@ export const useNostrStore = create<State & Actions>()((set, get) => ({
     get().removeFromSubList(subId);
   },
   purgeEvents: (subId) => {
-    const subList = get().subList;
-    const purgingSub = subList.find((sub) => sub.id === subId);
+    const subMap = get().subMap;
+    const purgingSub = subMap[subId];
     if (!purgingSub) return;
 
     const purgingFilters = purgingSub.filters;
-    const otherSubsWithSameFilters = subList.filter((otherSub) => {
-      if (otherSub.id === subId) return false;
-
-      const otherSubFilters = otherSub.filters;
-      return _.isEqual(purgingFilters, otherSubFilters);
-    });
-    if (otherSubsWithSameFilters.length > 0) return;
+    const foundAnotherSubWithSameFilters = _.find(
+      subMap,
+      (sub, key) => key !== subId && _.isEqual(sub.filters, purgingFilters)
+    );
+    if (foundAnotherSubWithSameFilters) return;
 
     set((store) => ({
       events: store.events.filter((event) => !matchFilters(purgingFilters, event)),
@@ -67,17 +67,17 @@ export const useNostrStore = create<State & Actions>()((set, get) => ({
   },
   setIsBatching: (isBatching) => set({ isBatching: isBatching }),
   addToQueue: ({ filters, relays }, subId) => {
-    set((store) => ({
-      queue: [...store.queue, { config: { filters, relays }, subId }],
-    }));
+    set((store) => ({ queue: { ...store.queue, [subId]: { filters, relays } } }));
   },
-  clearQueue: () => set({ queue: [] }),
+  clearQueue: () => set({ queue: {} }),
   addEvent: (event) => set((store) => ({ events: [...store.events, event] })),
   addToSubList: (subId, filters) => {
-    set((store) => ({ subList: [...store.subList, { id: subId, filters, eose: false }] }));
+    set((store) => ({
+      subMap: { ...store.subMap, [subId]: { filters, eose: false } },
+    }));
   },
   removeFromSubList: (subId) => {
-    set((store) => ({ subList: store.subList.filter((sub) => sub.id !== subId) }));
+    set((store) => ({ subMap: _.omit(store.subMap, subId) }));
   },
   handlePoolSub: ({ filters, relays }, subIds) => {
     const pool = get().pool;
@@ -90,22 +90,21 @@ export const useNostrStore = create<State & Actions>()((set, get) => ({
   },
   processQueue: () => {
     const queue = get().queue;
-    if (queue.length > 0) {
-      const flattenSub = queue.reduce<{ filters: Filter[]; relays: string[] }>(
-        (acc, sub) => ({
-          filters: [...acc.filters, ...sub.config.filters],
-          relays: [...acc.relays, ...sub.config.relays],
-        }),
-        { filters: [], relays: [] }
-      );
+    if (_.isEmpty(queue)) return;
 
-      get().handlePoolSub(
-        { filters: flattenSub.filters, relays: flattenSub.relays },
-        queue.map((sub) => sub.subId)
-      );
-      get().clearQueue();
-      get().setIsBatching(false);
-    }
+    const { filters, relays } = _.reduce(
+      queue,
+      (acc, { filters, relays }) => {
+        acc.filters.push(...filters);
+        acc.relays.push(...relays);
+        return acc;
+      },
+      { filters: [] as Filter[], relays: [] as string[] }
+    );
+
+    get().handlePoolSub({ filters, relays }, _.keys(queue));
+    get().clearQueue();
+    get().setIsBatching(false);
   },
   handleNewSub: ({ filters, relays, options }, subId) => {
     get().addToSubList(subId, filters);
