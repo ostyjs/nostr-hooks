6f36381bdf37a6d9f762462419742d24cb55ae15
Sepehr Safari <safari.sepehr@gmail.com>
2024-02-28 17:50:40 +0330

    add comments for useNdk hook

--- src/hooks/use-ndk/index.ts
+++ src/hooks/use-ndk/index.ts
@@ -2,6 +2,10 @@ import { useContext } from 'react';
 
 import { NostrHooksContext, NostrHooksContextType } from '../../contexts';
 
+/**
+ * Custom hook for accessing the ndk instance and its setter.
+ * @returns An object containing the ndk instance and its setter.
+ */
 export const useNdk = () => {
   const { ndk, setNdk } = useContext(NostrHooksContext) as NostrHooksContextType;
 
