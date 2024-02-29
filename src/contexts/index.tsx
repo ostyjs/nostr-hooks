97aed95e1b0056f86237e9b18a2fc9d1a3a85144
Sepehr Safari <safari.sepehr@gmail.com>
2024-02-29 03:42:37 +0330

    add more relays

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,7 +1,22 @@
 import NDK from '@nostr-dev-kit/ndk';
 import { createContext, useEffect, useState } from 'react';
 
-const initialNdk = new NDK({ explicitRelayUrls: ['wss://nos.lol'] });
+const initialNdk = new NDK({
+  explicitRelayUrls: [
+    'wss://nos.lol',
+    'wss://relay.nostr.band',
+    'wss://relay.damus.io',
+    'wss://relay.snort.social',
+    'wss://relayable.org',
+    'wss://offchain.pub',
+    'wss://purplepag.es',
+    'wss://relay.primal.net',
+    'wss://atlas.nostr.land',
+    'wss://eden.nostr.land',
+    'wss://relay.noswhere.com',
+    'wss://relay.nostr.bg',
+  ],
+});
 
 export type NostrHooksContextType = { ndk: NDK; setNdk: (ndk: NDK) => void };
 
