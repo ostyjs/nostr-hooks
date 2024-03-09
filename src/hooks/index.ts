38f6e4f5f04f59938d9156e3f28412757052e85d
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-09 16:32:20 +0330

    export new hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export { useActiveUser } from './use-active-user';
 export { useNdk } from './use-ndk';
 export { useNewEvent } from './use-new-event';
 export { useNip07 } from './use-nip07';
+export { useNostrHooks } from './use-nostr-hooks';
 export { useProfiles } from './use-profiles';
 export { usePublish } from './use-publish';
 export { useSubscribe } from './use-subscribe';
