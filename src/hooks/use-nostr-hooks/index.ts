342b60ac74dbc4ffffa18bda56eed0c8b70ec797
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-09 16:14:15 +0330

    add use-nostr-hooks as the initializer

---
+++ src/hooks/use-nostr-hooks/index.ts
@@ -0,0 +1,18 @@
+import NDK from '@nostr-dev-kit/ndk';
+import { useEffect } from 'react';
+
+import { useNdk } from '../use-ndk';
+
+export const useNostrHooks = ({ initialNdk }: { initialNdk?: NDK | undefined }) => {
+  const { ndk, setNdk } = useNdk();
+
+  useEffect(() => {
+    ndk.connect();
+  }, [ndk]);
+
+  useEffect(() => {
+    if (initialNdk) {
+      setNdk(initialNdk);
+    }
+  }, [initialNdk]);
+};
