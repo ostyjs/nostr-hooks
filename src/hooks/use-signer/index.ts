4eebfdb97f8ff148fb17528b70487d5a5a165cf5
sepehr-safari <safari.sepehr@gmail.com>
2024-08-13 23:36:44 +0330

    fix setSigner bug

--- src/hooks/use-signer/index.ts
+++ src/hooks/use-signer/index.ts
@@ -1,10 +1,21 @@
+import { NDKSigner } from '@nostr-dev-kit/ndk';
+import { useCallback } from 'react';
+
 import { useStore } from '../../store';
 
 export const useSigner = () => {
   const ndk = useStore((state) => state.ndk);
-  const setSigner = useStore((state) => state.setSigner);
+  const setNdk = useStore((state) => state.setNdk);
 
   const signer = ndk.signer;
+
+  const setSigner = useCallback(
+    (signer: NDKSigner | undefined) => {
+      ndk.signer = signer;
+      setNdk(ndk);
+    },
+    [ndk, setNdk]
+  );
 
   return { signer, setSigner };
 };
