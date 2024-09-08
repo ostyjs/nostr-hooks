34bc2d43748a6adc8b904c07a3b9958f360b7f0f
sepehr-safari <safari.sepehr@gmail.com>
2024-09-08 17:54:39 +0330

    add custom ndk to use-auto-login hook

--- src/hooks/use-auto-login/index.ts
+++ src/hooks/use-auto-login/index.ts
@@ -1,9 +1,20 @@
+import NDK from '@nostr-dev-kit/ndk';
 import { useEffect } from 'react';
 
 import { useLogin } from '../use-login';
 
-export const useAutoLogin = () => {
-  const { loginFromLocalStorage } = useLogin();
+type Params = {
+  customNdk: NDK;
+  setCustomNdk: (customNdk: NDK) => void;
+};
+
+/**
+ * Custom hook for automatic login functionality based on previously stored login method in local storage.
+ *
+ * @param params - Optional parameters for custom NDK instance and its setter function.
+ */
+export const useAutoLogin = (params?: Params) => {
+  const { loginFromLocalStorage } = useLogin(params);
 
   useEffect(() => {
     loginFromLocalStorage();
