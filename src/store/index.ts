23d9ce348b8308a0654a8384eb4cc93b981832bc
sepehr-safari <safari.sepehr@gmail.com>
2024-11-19 20:59:04 +0330

    implement dynamic create store method

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,56 @@
-import NDK from '@nostr-dev-kit/ndk';
+import NDK, {
+  NDKConstructorParams,
+} from '@nostr-dev-kit/ndk';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-type NDKState = {
-  ndk: NDK;
+type State = {
+  // ndk state
+  constructorParams: NDKConstructorParams | undefined;
+
+  ndk: NDK | undefined;
 };
 
-type NDKActions = {
-  setNdk: (ndk: NDK) => void;
+type Actions = {
+  // ndk actions
+  initNdk: (constructorParams?: NDKConstructorParams) => void;
+
+  setSigner: (signer: NDKSigner | undefined) => void;
+
 };
 
-export const useStore = create<NDKState & NDKActions>()((set) => ({
-  ndk: new NDK({
-    explicitRelayUrls: ['wss://nos.lol'],
-  }),
+export const createStore = (storeName: string) => {
+  return create<State & Actions>()(
+    persist(
+      (set, get) => ({
+        // ndk state
+        constructorParams: undefined,
 
-  setNdk: (ndk) => set({ ndk }),
-}));
+        ndk: undefined,
+
+        // ndk actions
+        initNdk: (constructorParams) => {
+          const ndk = new NDK(constructorParams);
+
+          set({ constructorParams, ndk });
+        },
+
+        setSigner: (signer) => {
+          const newConstructorParams = { ...get().constructorParams };
+
+          if (!signer) {
+            delete newConstructorParams.signer;
+          } else {
+            newConstructorParams.signer = signer;
+          }
+
+          get().initNdk(newConstructorParams);
+        },
+      }),
+      {
+        name: storeName || 'ndk-store',
+        partialize: (state) => ({ loginData: state.loginData }),
+      }
+    )
+  );
+};
