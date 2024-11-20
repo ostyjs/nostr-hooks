0fba66445577df85ef69cf0f52331ca1ee1bf66a
sepehr-safari <safari.sepehr@gmail.com>
2024-11-20 11:32:56 +0330

    remove ndk and signer hooks

--- src/hooks/use-ndk/index.ts
+++
@@ -1,8 +0,0 @@
-import { useStore } from '../../store';
-
-export const useNdk = () => {
-  const ndk = useStore((state) => state.ndk);
-  const setNdk = useStore((state) => state.setNdk);
-
-  return { ndk, setNdk };
-};
