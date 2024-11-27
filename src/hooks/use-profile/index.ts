75b4eafb9f5ce22e131b35f8cfd11ac4f1152839
sepehr-safari <safari.sepehr@gmail.com>
2024-11-27 12:41:23 +0330

    fix & enhance use-profile hook

--- src/hooks/use-profile/index.ts
+++ src/hooks/use-profile/index.ts
@@ -1,5 +1,7 @@
-import NDK, { NDKUserProfile } from '@nostr-dev-kit/ndk';
+import { NDKUserProfile } from '@nostr-dev-kit/ndk';
 import { useEffect, useState } from 'react';
+
+import { useNdk } from '../use-ndk';
 
 type ProfileParams = {
   nip05?: string;
@@ -12,25 +14,28 @@ type ProfileParams = {
 /**
  * Custom hook for fetching a user profile.
  *
- * @param ndk - NDK instance to use for fetching the user profile.
  * @param profileParams - Optional parameters for fetching the user profile.
- * @returns An object containing the user profile or null.
+ * @returns An object containing the user profile or undefined.
  */
-export const useProfile = (ndk: NDK | undefined, profileParams?: ProfileParams) => {
-  const [profile, setProfile] = useState<NDKUserProfile | null>(null);
+export const useProfile = (profileParams?: ProfileParams) => {
+  const [profile, setProfile] = useState<NDKUserProfile | undefined>(undefined);
+
+  const { ndk } = useNdk();
 
   useEffect(() => {
     if (!profileParams) return;
     if (profileParams.constructor === Object && Object.keys(profileParams).length === 0) return;
+    if (!profileParams.nip05 && !profileParams.pubkey && !profileParams.npub) return;
     if (!ndk) return;
 
     ndk
       .getUser(profileParams)
       .fetchProfile()
       .then((profile) => {
-        setProfile(profile);
+        setProfile(profile || undefined);
       });
   }, [
+    setProfile,
     profileParams?.nip05,
     profileParams?.pubkey,
     profileParams?.npub,
