e83a238a047aa3b2565d38ca31f2ef96c30e4383
sepehr-safari <safari.sepehr@gmail.com>
2024-12-18 13:07:19 +0330

    feat: export useFollows hook from index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useActiveUser } from './use-active-user';
+export { useFollows } from './use-follows';
 export { useLogin } from './use-login';
 export { useNdk } from './use-ndk';
 export { useProfile } from './use-profile';
